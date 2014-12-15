a46c6e2b11c571ac84d82c82567201ed5cc1e429
Andy Wilkinson <awilkinson@pivotal.io>
2014-12-15 16:45:58 +0000

    Upgrade to latest versions of Spring AMQP and Spring Integration
    See spring-io/platform#117 and spring-io/platform#118

--- platform-foundation-tests/src/test/groovy/io/spring/platform/foundation/BomTests.groovy
+++ platform-foundation-tests/src/test/groovy/io/spring/platform/foundation/BomTests.groovy
@@ -38,7 +38,7 @@ public class BomTests {
 
 	@Test
 	void versionPropertiesAreNotOverridden() {
-		def expectedOverrides = ['junit.version']
+		def expectedOverrides = ['spring-amqp.version', 'spring-integration.version', 'junit.version']
 		def versions = extractVersionsFromPom(bootDependenciesPom)
 
 		def overriddenVersions = [:]
