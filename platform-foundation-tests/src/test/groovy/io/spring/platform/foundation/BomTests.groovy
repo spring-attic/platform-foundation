0c7e6f3b27e476e4f4bd5a1cb4ff55e324efc229
Andy Wilkinson <awilkinson@pivotal.io>
2015-01-12 12:37:29 +0000

    Upgrade to Spring Boot 1.2.1.RELEASE
    See spring-io/platform#129

--- platform-foundation-tests/src/test/groovy/io/spring/platform/foundation/BomTests.groovy
+++ platform-foundation-tests/src/test/groovy/io/spring/platform/foundation/BomTests.groovy
@@ -38,7 +38,7 @@ public class BomTests {
 
 	@Test
 	void versionPropertiesAreNotOverridden() {
-		def expectedOverrides = ['spring-amqp.version', 'spring-integration.version', 'junit.version']
+		def expectedOverrides = [ 'junit.version' ]
 		def versions = extractVersionsFromPom(bootDependenciesPom)
 
 		def overriddenVersions = [:]
